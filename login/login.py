28bc05320daf4bcab5d64a240ef5696a395be9f1
Igna <ignaao@gmail.com>
2022-10-16 16:13:37 -0300

    refactor
    refactor de login.py que ahora usa clases y metodos más básicos. Users es el módulo de más alto nivel y se ocupa de las validaciones. Tiene el inconveniente que así como está armado no es fácil armar tests automáticos. Por ahora quedará así

--- login/login.py
+++ login/login.py
@@ -1,14 +1,15 @@
 #!/usr/bin/env python3
-# -*- coding: utf-8 -*-
+# login# -*- coding: utf-8 -*-
 """
 Created on Mon Jul 25 22:17:33 2022
 
 @author: igna
+
+refactor: Sun Oct 16 16:00:00
 """
 
 import os
 from hashlib import sha256
-from pwinput import pwinput
 
 from login.sqlpasswd import create_connection
 from login.sqlpasswd import execute_query
@@ -17,7 +18,6 @@ from login.sqlpasswd import add_new_user_query
 
 BASE_PATH = os.path.dirname(os.path.dirname(__file__))
 DATA_PATH = os.path.join(BASE_PATH, "data")
-
 DATA_BASE = os.path.join(DATA_PATH, "passwords.sqlite")
 
 connection = create_connection(DATA_BASE)
@@ -33,70 +33,93 @@ CREATE TABLE IF NOT EXISTS users (
 execute_query(connection, create_users_table)
 
 
-def check_user_indb(user: str) -> bool:
-    """
-    Checks if the given user already exists in the sql table.
-    """
-    query = f"SELECT name FROM users WHERE name = '{user};'"
-    query_result = execute_read_query(connection, query)
-    if len(query_result) == 1:
-        return True
+class UsersDB:
+    def __init__(self, user):
+        self.user = user
+        self.user_exists = False
+        self.creation_status = False
+        self.passwdvalidation = False
 
+    def get_user_from_db(self) -> bool:
+        """
+        Checks if the given user already exists in the sql table.
+        """
+        query = f"SELECT name FROM users WHERE name = '{self.user}';"
+        query_result = execute_read_query(connection, query)
+        print(query_result)
+        if len(query_result) == 1:
+            self.user_exists = True
 
-def create_user_indb(user: str) -> bool:
-    """
-    Creates a password and saves it encrypted in sql table.
-    Returns False if conditions don't match and True if they do
-    """
-    # Checking users existance
-    flag = check_user_indb(user)
-    if flag:
-        print(f"User {user} already exists.")
-        return False
-    # Password input and encode to utf-8
-    passwd = pwinput(prompt="Ingrese una password: ").encode("utf-8")
-    passwd_check = pwinput(prompt="Ingrese la password otra vez: ").encode(
-        "utf-8"
-    )
-    if passwd != passwd_check:
-        print("Las contraseñas no coinciden, vuelva a intentarlo.")
-        return False
-    else:
+    def add_user_to_db(self, passwd: bytes) -> None:
+        """
+        Adds the user and hashed passwd to the sql table.
+        Returns False if conditions don't match and True if they do
+        """
         # Encryptation of the password using sha256 algorithm
         encrypted_passwd = sha256(passwd).hexdigest()
-        # Saving the password in the sqlite table
-        query = add_new_user_query(user, encrypted_passwd)
+        # Saving the user and password in the sqlite table
+        query = add_new_user_query(self.user, encrypted_passwd)
         execute_query(connection, query, verbose=True)
-        return True
+        self.creation_status = True
+
+    def delete_user_indb(self) -> None:
+        """
+        Deletes the row in the sql table that stores that user
+        """
+        query = f"DELETE FROM users WHERE name = '{self.user}';"
+        execute_query(connection, query)
+        self.user_exists = False
+
+    def change_pass_indb(self, new_passwd: bytes) -> None:
+        """
+        Changes the password hash of a given user
+        """
+        hash = sha256(new_passwd).hexdigest()
+        query = (
+            f"UPDATE users SET passwd = '{hash}' WHERE name = '{self.user}';"
+        )
+        execute_query(connection, query)
+
+    def passwd_validation_indb(self, passwd: bytes) -> bool:
+        """
+        Checks if the given passwd is correct
+        """
+        query = f"SELECT passwd FROM users WHERE name = '{self.user}';"
+        query_result = execute_read_query(connection, query)[0][0]
+        # Password to validate is hashed in order to be compared with the
+        # one in passwd_list.key file.
+        hash_pass = sha256(passwd).hexdigest()
+        # The password hashed and given as input is compared with the hashed
+        # stored one, if they match, a True flag is returned
+        if query_result == hash_pass:
+            self.passwdvalidation = True
 
 
-def delete_user_indb(user: str) -> None:
-    """
-    Deletes the row in the sql table that stores that user
-    """
-    query = f"DELETE FROM users WHERE name = '{user}';"
-    execute_query(connection, query)
+class UsersDirs:    
+    def __init__(self, user):
+        self.user = user
+        self.dirname = user + "USR"
+        self.absdirname = DATA_PATH + "/" + self.dirname
 
+    def get_user_cwd(self) -> None:
+        return os.getcwd()
 
-def change_pass_indb(user: str, new_passwd: bytes) -> None:
-    """
-    Changes the password hash of a given user
-    """
-    hash = sha256(new_passwd).hexdigest()
-    query = f"UPDATE users SET passwd = '{hash}' WHERE name = '{user}';"
-    execute_query(connection, query)
-
-
-def passwd_validation_indb(user: str, passwd: bytes) -> bool:
-    """
-    Checks if the given passwd is correct
-    """
-    query = f"SELECT passwd FROM users WHERE name = '{user}';"
-    query_result = execute_read_query(connection, query)[0][0]
-    # Password to validate is hashed in order to be compared with the
-    # one in passwd_list.key file.
-    hash_pass = sha256(passwd).hexdigest()
-    # The password hashed and given as input is compared with the hashed
-    # stored one, if they match, a True flag is returned
-    if query_result == hash_pass:
-        return True
+    def create_user_dir(self) -> None:
+        """creates the directory for the user's accounts"""
+        os.makedirs(self.absdirname)
+    
+    def delete_user_dir(self) -> None:
+        """Deletes the directory and all its content"""
+        try:
+            os.rmdir(self.absdirname)
+        except OSError:
+            from shutil import rmtree
+            rmtree(self.absdirname)
+    
+    def login(self) -> None:
+        """Changes the current working directory to the one of the user"""
+        os.chdir(self.absdirname)
+    
+    def logout(self) -> None:
+        """Chages the current working directory to tha data directory"""
+        os.chdir(DATA_PATH)
--- source/users.py
+++ source/users.py
@@ -8,8 +8,11 @@ Módulo para condensar las funciones relacionadas con cuentas
 
 crear_usuario()
 eliminar_usuario()
+cambiar_contraseña()
 iniciar_sesion()
 cerrar_sesion()
+
+refactor: Sun Oct 16 16:00:00
 """
 import os
 
@@ -17,12 +20,7 @@ import pandas as pd
 from pwinput import pwinput
 
 from source.info import info
-from source.misc import extra_char_cleaner
-from login.login import create_user_indb
-from login.login import delete_user_indb
-from login.login import change_pass_indb
-from login.login import passwd_validation_indb
-
+from login.login import UsersDirs, UsersDB
 
 BASE_PATH = os.path.dirname(os.path.dirname(__file__))
 DATA_PATH = os.path.join(BASE_PATH, "data")
@@ -30,50 +28,103 @@ os.chdir(DATA_PATH)
 pd.set_option("display.max_columns", 11)
 
 
-def crear_usuario():
-    """Creates the user's directory where all accounts will be stored"""
-    if os.getcwd() != DATA_PATH:
+def crear_usuario() -> None:
+    """
+    Creates an user with its directories and entries in the data base.
+    Uses inputs for name of the user and password.
+    Won't allow you to create an user if it already existis or if you are
+    logged in with another users.s
+    """
+    if os.getcwd() == DATA_PATH:
+        username = input("\nIngrese el nombre de usuario\n")
+        user_dir = UsersDirs(username)
+        user_db = UsersDB(username)
+        if not os.path.isdir(user_dir.absdirname):
+            # Password input and encode to utf-8
+            password = pwinput(prompt="Ingrese una password: ").encode("utf-8")
+            password_check = pwinput(
+                prompt="Ingrese la password otra vez: "
+            ).encode("utf-8")
+            if password == password_check:
+                user_dir.create_user_dir()
+                user_db.add_user_to_db(passwd=password)
+                if user_db.creation_status:
+                    print(f"\nSe creo el usuario '{username}'\n")
+                else:
+                    print(
+                        "\nFalló la creación del usuario en la base de datos\n"
+                    )
+            else:
+                print("Las contraseñas no coinciden, vuelva a intentarlo.")
+        else:
+            print(f"\nYa existe el usuario '{username}'\n")
+    else:
         print(
             "No se puede crear usuario si está iniciada una sesión",
             "Asegúrese de haber deslogueado y vuelva a intentar.",
         )
-    elif os.getcwd() == DATA_PATH:
-        folder_name = input("\nIngrese el nombre de usuario\n") + "USR"
-        user_name = extra_char_cleaner(folder_name)
-        if os.path.isdir(folder_name):
-            print(f"\nYa existe el usuario '{user_name}'\n")
-        else:
-            creation_status = create_user_indb(user_name)
-            if creation_status:
-                os.makedirs(folder_name)
-                print(f"\nSe creo el usuario '{user_name}'\n")
-            else:
-                print("\nFalló la creación del usuario en la base de datos\n")
 
 
-def cambiar_contraseña():
-    """Changes the password of a given user"""
-    if os.getcwd() != DATA_PATH:
-        print(
-            "No se puede cambiar la contraseña si el usuario esta logueado",
-            "Asegúrese de haber deslogueado y vuelva a intentar.",
-        )
-    elif os.getcwd() == DATA_PATH:
-        folder_name = input("\nIngrese el nombre de usuario\n") + "USR"
-        user_name = extra_char_cleaner(folder_name)
-        if os.path.isdir(folder_name):
-            old_passwd = pwinput("\nIngrese la contraseña actual: ").encode(
-                "utf-8"
-            )
-            if passwd_validation_indb(user_name, old_passwd):
-                new_passwd = pwinput("\nIngrese la nueva contraseña: ").encode(
-                    "utf-8"
+def eliminar_usuario() -> None:
+    """
+    Deletes an user and all its information stored in its directories and
+    data base.
+    Ues inputs for name of the user and password.
+    Won't allow you to delete an user if you are logged in with that or
+    another user.
+    """
+    if os.getcwd() == DATA_PATH:
+        username = input("\nIngrese el nombre de usuario a borrar\n")
+        user_dir = UsersDirs(username)
+        user_db = UsersDB(username)
+        if os.path.isdir(user_dir.absdirname):
+            password = pwinput("Enter password: ").encode("utf-8")
+            user_db.passwd_validation_indb(passwd=password)
+            if user_db.passwdvalidation:
+                warning = (
+                    f"¿Seguro que queres eliminar el usuario '{username}'?\n\n"
+                    "todos los datos contenidos en él se perderán para siempre.\n\n"
+                    "Ingrese '1', 'si' o 'y' para borrar\n"
+                    "Ingrese cualquier otra cosa para cancelar\n"
                 )
-                check_new_passwd = pwinput(
+                user_answer = input(warning)
+                valid_answers = ["1", "si", "y"]
+                if user_answer in valid_answers:
+                    user_dir.delete_user_dir()
+                    user_db.delete_user_indb()
+                else:
+                    print(f"\nNo se eliminó el usuario {username}\n")
+            else:
+                print("\nPassword incorrecta\n")
+        else:
+            print(f"\nNo existe el usuario {username}\n")
+    else:
+        print("Debes cerrar sesion para eliminar un usuario.")
+
+
+def cambiar_contraseña() -> None:
+    """
+    Changes the password of a given user.
+    Uses inputs for name of the user, old password and new password.
+    """
+    if os.getcwd() == DATA_PATH:
+        username = input(
+            "\nIngrese el nombre de usuario para cambiar passwd.\n"
+        )
+        user_dir = UsersDirs(username)
+        user_db = UsersDB(username)
+        if os.path.isdir(user_dir.absdirname):
+            old_password = pwinput("\nIngrese la contraseña actual: ").encode("utf-8")
+            user_db.passwd_validation_indb(passwd=old_password)
+            if user_db.passwdvalidation:
+                new_password = pwinput(
+                    "\nIngrese la nueva contraseña: "
+                ).encode("utf-8")
+                check_new_password = pwinput(
                     "\nIngrese nuevamente la nueva contraseña: "
                 ).encode("utf-8")
-                if new_passwd == check_new_passwd:
-                    change_pass_indb(user_name, new_passwd)
+                if new_password == check_new_password:
+                    user_db.change_pass_indb(new_passwd=new_password)
                     print("\nLa contraseña fue cambiada con exito\n")
                 else:
                     print(
@@ -82,75 +133,37 @@ def cambiar_contraseña():
             else:
                 print("\nPassword incorrecta, vuelva a intentarlo\n")
         else:
-            print(f"\nNo existe el usuario {user_name}\n")
-
-
-def eliminar_usuario():
-    """
-    Delets the directory of the given user, including all data stored in it
-    """
-    if os.getcwd() != DATA_PATH:
-        print("Debes cerrar sesion para eliminar un usuario.")
-    elif os.getcwd() == DATA_PATH:
-        folder_name = (
-            input("\nIngrese el nombre de usuario a borrar\n") + "USR"
-        )
-        user_name = extra_char_cleaner(folder_name)
-        if os.path.isdir(folder_name):
-            passwd = pwinput("Enter password: ").encode("utf-8")
-            if passwd_validation_indb(user_name, passwd):
-                advertencia = (
-                    f"¿Seguro que queres eliminar el usuario '{user_name}'?\n\n"
-                    "todos los datos contenidos en él se perderán para siempre.\n\n"
-                    "Ingrese '1', 'si' o 'y' para borrar\n"
-                    "Ingrese cualquier otra cosa para cancelar\n"
-                )
-                respuesta = input(advertencia)
-                posibles_respuestas = ["1", "si", "y"]
-                if respuesta in posibles_respuestas:
-                    try:
-                        os.rmdir(folder_name)
-                        print(f"\nSe eliminó el usuario {user_name}\n")
-                    except OSError:
-                        from shutil import rmtree
-                        rmtree(folder_name)
-                        print(
-                            f"\nSe eliminó el usr. {user_name} y todos sus datos."
-                        )
-                    finally:
-                        delete_user_indb(user_name)
-                else:
-                    print(f"\nNo se eliminó el usuario {user_name}\n")
-            else:
-                print("\nPassword incorrecta\n")
-        else:
-            print(f"\nNo existe el usuario {user_name}\n")
-
-
-def iniciar_sesion():
-    """Changes the current working directory to the user's directory"""
-    if "USR" in os.getcwd():
-        print("Ya hay una sesión iniciada")
+            print(f"\nNo existe el usuario {username}\n")
     else:
-        folder_name = input("\nNombre de usuario\n") + "USR"
-        user_name = extra_char_cleaner(folder_name)
-        if os.path.isdir(folder_name):
-            path = os.path.join(DATA_PATH, folder_name)
-            passwd = pwinput("Enter password: ").encode("utf-8")
-            if passwd_validation_indb(user_name, passwd):
-                os.chdir(path)
-                print(f"\nInicio de sesion de {user_name}\n")
-                info(verbose=True)
+        print("\nDebes cerrar sesion para cambiar una contraseña.\n")
+
+
+def iniciar_sesion(verbose=True) -> None:
+    """Changes the current working directory to the user's directory"""
+    if os.getcwd() == DATA_PATH:
+        username = input("\nNombre de usuario\n")
+        user_dir = UsersDirs(username)
+        user_db = UsersDB(username)
+        if os.path.isdir(user_dir.absdirname):
+            password = pwinput("Enter password: ").encode("utf-8")
+            user_db.passwd_validation_indb(password)
+            if user_db.passwdvalidation:
+                os.chdir(user_dir.absdirname)
+                print(f"\nInicio de sesion de {username}\n")
+                info(verbose=verbose)
             else:
                 print("Contraseña incorrecta")
         else:
-            print(f"\nNo existe el usuario '{user_name}'\n")
-
+            print(f"\nNo existe el usuario '{username}'\n")
+    elif "USR" in os.getcwd():
+        print("\nYa hay una sesión iniciada.\n")
+    else:
+        print(f"\nUbicación inválida: {os.getcwd()}\n")
 
 def cerrar_sesion():
     """Changes the current working directory to the base directory"""
-    if "USR" not in os.getcwd():
-        print("No hay ninguna sesión iniciada.")
-    else:
+    if "USR" in os.getcwd():
         os.chdir(DATA_PATH)
         print("\nSe ha cerrado sesión\n")
+    else:
+        print("\nNo hay ninguna sesión iniciada.\n")
