b3564322c83e38ff4594d6bf3ea7363a09117006
Igna <ignaao@gmail.com>
2023-03-02 23:05:35 -0300

    minor
    ahora se le pueden pasar argumentos a create_account y delete_account

--- source/accounts.py
+++ source/accounts.py
@@ -13,23 +13,27 @@ delete_account()
 from source.account_core import AccountsCreator
 
 
-def create_account():
+def create_account(name_acc: str = None, currency_acc: str = None) -> None:
     """Creates a .txt file which name will be the account name"""
-    name_acc = input("\nIntroduzca el nombre para la nueva cuenta\n")
-    currency_acc = input(
-        "\nIngresar el tipo de moneda, por ejemplo: ARS, USD, etc\n"
-    ).upper()
+    if not name_acc:
+        name_acc = input("\nIntroduzca el nombre para la nueva cuenta\n")
+    if not currency_acc:
+        currency_acc = input(
+            "\nIngresar el tipo de moneda, por ejemplo: ARS, USD, etc\n"
+        ).upper()
     account = AccountsCreator(acc_name=name_acc, acc_currency=currency_acc)
     account.add_account()
     print(f"\nSe ha creado la cuenta '{name_acc}'\n")
 
 
-def delete_account():
+def delete_account(name_acc: str = None, currency_acc: str = None) -> None:
     """Deletes the .txt file of the given account name"""
-    name_acc = input("\nIntroduzca el nombre de la cuenta a eliminar\n")
-    currency_acc = input(
-        "\nIntroduzca el tipo de moneda usada en la cuenta (ARS, USD, etc)\n"
-    ).upper()
+    if not name_acc:
+        name_acc = input("\nIntroduzca el nombre de la cuenta a eliminar\n")
+    if not currency_acc:
+        currency_acc = input(
+            "\nIntroduzca el tipo de moneda usada en la cuenta (ARS, USD, etc)\n"
+        ).upper()
     account = AccountsCreator(acc_name=name_acc, acc_currency=currency_acc)
     if account.exists:
         warning = "¿Seguro que queres eliminar la cuenta?\n\n\
